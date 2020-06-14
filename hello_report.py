62b1d277cba66c19404e059163f43dec82b747fb
Danylo Halaiko <dan.galaiko@gmail.com>
2020-06-14 17:57:56 +0300

    Project has been done.

--- hello_report.py
+++ hello_report.py
@@ -1,8 +1,30 @@
 from reportlab.graphics import renderPDF
-from reportlab.graphics.shapes import Drawing, String
+from reportlab.graphics.charts.lineplots import LinePlot
+from reportlab.graphics.shapes import *
 
-d = Drawing(100, 100)
-s = String(50, 50, "Hello, world!", textAnchor="middle")
+import pars_work
 
-d.add(s)
-renderPDF.drawToFile(d, 'hello.pdf', 'A simple PDF file')
+URL = 'ftp://ftp.swpc.noaa.gov/pub/weekly/Predict.txt'
+data = pars_work.get_data(URL)
+
+pred = [row[2] for row in data]
+high = [row[3] for row in data]
+low = [row[4] for row in data]
+times = [row[0] + row[1] / 12.0 for row in data]
+d = Drawing(400, 200)
+
+lp = LinePlot()
+lp.x = 50
+lp.y = 50
+lp.height = 125
+lp.width = 300
+lp.data = [list(zip(times, pred)), list(zip(times, high)), list(zip(times, low))]
+
+lp.lines[0].strokeColor = colors.blue
+lp.lines[1].strokeColor = colors.red
+lp.lines[2].strokeColor = colors.green
+
+d.add(lp)
+d.add(String(250, 150, 'Sunspots', fillColor=colors.red))
+
+renderPDF.drawToFile(d, 'report1.pdf', 'Sunspots')
--- pars_work.py
+++ pars_work.py
@@ -1,23 +1,28 @@
-def get_data(filename):
-    with open(filename) as file:
+from urllib.request import urlopen
+
+COMMENT_CHARS = '#:'
+
+
+def get_data(url):
+    with urlopen(url) as file:
         data = []
         for line in file:
-            parsed_line = get_tuple(line)
+            parsed_line = get_tuple(line.decode())
             if parsed_line is not None:
                 data.append(parsed_line)
         return data
 
 
 def get_tuple(line):
-    if line[0] == '#' or line[0] == ':':
+    if line[0] in COMMENT_CHARS:
         return None
 
     words = line.split()
     data_list = []
     for x in range(5):
-        data_list.append(words[x])
+        data_list.append(float(words[x]))
     return tuple(data_list)
 
 
 if __name__ == '__main__':
-    print(get_data("Predict.txt"))
+    print(get_data("ftp://ftp.swpc.noaa.gov/pub/weekly/Predict.txt"))
